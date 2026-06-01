813a76f00d46e0994bb75cec163139643c373072
Isaque Pinheiro <isaquesp@gmail.com>
2026-06-01 18:04:55 -0300

    docs: standardize and update headers in all .pas files to MIT

--- Source/Interfaces/Nidus.Pooling.Interfaces.pas
+++ Source/Interfaces/Nidus.Pooling.Interfaces.pas
@@ -1,3 +1,16 @@
+{
+  ------------------------------------------------------------------------------
+  Nidus
+  Modular and scalable application framework for Delphi, inspired by the architectural patterns of NestJS.
+
+  SPDX-License-Identifier: MIT
+  Copyright (c) 2025-2026 Isaque Pinheiro
+
+  Licensed under the MIT License.
+  See the LICENSE file in the project root for full license information.
+  ------------------------------------------------------------------------------
+}
+
 unit Nidus.Pooling.Interfaces;
 
 interface
